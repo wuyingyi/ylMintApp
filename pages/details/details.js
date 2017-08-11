8a3f75c43ee046d1972ae5910d7cdebc56dcbf43
wuyingyi <wuyingyi@99114.com>
2017-08-11 18:01:17 +0800

    详情页

--- pages/details/details.js
+++ pages/details/details.js
@@ -9,11 +9,23 @@ Page({
     praise: false,
     collect: false,
     arrs: [],
+    contentBtm:true,
+    showAuthor:false,
+    showTime:false,
+    showCode:false,
+    author:"——Y",
+    times:"2017-8-11 星期五 雨",
     id: "001", 
     imgurl: "../../image/top.jpg", 
     address: "上海",
      name: '一季花开、一季花落',
      content: '人生最可怕的事，是一边后悔一边生活。知道那么多大道理，却依然过不好这一生。\r\n反过来想想，也许正是因为我们知道的大道理既不够多，也不够深，才让我们无法在思想中融会贯通，形成一套自己的思维习惯和行为习惯，从而在自相矛盾的大道理中无法前行。',
+     items: [
+       { name: 'signature', value: '是否显示签名' },
+       { name: 'time ', value: '是否显示时间' },
+       { name: 'code', value: '是否显示专属二维码' },
+      
+     ]
   },
 
   /**
@@ -32,6 +44,37 @@ Page({
     })
   },
 
+  checkboxChange: function (e) {
+    console.log('checkbox发生change事件，携带value值为：', e.detail.value)
+  },
+  //显示名字
+  showName:function(){
+    var showAuthor = this.data.showAuthor
+    this.setData({
+      showAuthor: !showAuthor
+    })
+  },
+  //显示时间
+  showTime: function () {
+    var showTime = this.data.showTime
+    this.setData({
+      showTime: !showTime
+    })
+  },
+ //显示二维码
+  showCode: function () {
+    var showCode = this.data.showCode
+    this.setData({
+      showCode: !showCode
+    })
+  },
+  //生成海报
+  createNews:function(){
+    var contentBtm = this.data.contentBtm
+    this.setData({
+      contentBtm:false
+    })
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
@@ -43,27 +86,6 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
   
   },
 
